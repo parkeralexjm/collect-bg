5f3049e54aed57bca9b581bbd48caff33780abf3
Alex Parker <parkeralexjm@gmail.com>
2023-10-05 22:57:42 +0100

    Roughtly styled left column, added top 10 hot games

--- frontend/src/components/GamesDisplay.js
+++ frontend/src/components/GamesDisplay.js
@@ -40,7 +40,7 @@ export default function GamesDisplay() {
         const { data } = await axios.get('https://boardgamegeek.com/xmlapi2/hot?type=boardgame&count=10') // This is unauthorised for testing
         const convertedData = new XMLParser().parseFromString(data)
         console.log(convertedData.children)
-        setTopGames(convertedData.children)
+        setTopGames(convertedData.children.slice(0, 10))
       } catch (error) {
         console.log(error)
       }
@@ -50,20 +50,32 @@ export default function GamesDisplay() {
 
   return (
     <section className='display-background'>
-      <div className='display'>
+      <div className='display container'>
         <Row className='d-flex'>
-          <Col xs={0} md={0} lg={2} className='left-col'>
+          <Col xs={0} md={0} lg={3} xxl={2} className='left-col'>
             <Link to={''}>
               <img className='logo' src={logo} alt="collect-dot-bg-logo" />
             </Link>
             {topGames.map((game, index) => {
               return (
                 <Fragment key={index}>
+                  <div className='display-top-games'>
+                    <img src={game.children[0].attributes.value} alt={game.children[1].attributes.value} />
+                    <div>
+                      <h3>{game.children[1].attributes.value}</h3>
+                      <h4>{game.children[2].attributes.value}</h4>
+                    </div>
+                  </div>
                 </Fragment>)
             })}
-            <p>Socials</p>
+            <div className='socials'>
+              <Link to='https://twitter.com/home'><i className="fa-brands fa-xl fa-x-twitter"></i></Link>
+              <Link to='https://www.facebook.com/'><i className="fa-brands fa-xl fa-facebook-f"></i></Link>
+              <Link to='https://github.com/parkeralexjm'><i className="fa-brands fa-xl fa-github"></i></Link>
+              <Link to='https://www.linkedin.com/in/parkeralexjm/'><i className="fa-brands fa-xl fa-linkedin"></i></Link>
+            </div>
           </Col>
-          <Col xs={12} md={9} lg={7} className='center-col'>
+          <Col xs={12} md={9} lg={6} xxl={7} className='center-col'>
             <p>Carousel</p>
             <p>Categories</p>
             <p>Games Display</p>
