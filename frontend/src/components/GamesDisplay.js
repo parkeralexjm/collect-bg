63551de0df5c8646a8838d13c1c7403360bf24e3
Alex Parker <parkeralexjm@gmail.com>
2023-10-05 19:16:46 +0100

    Added XML parser to convert data from API

--- frontend/src/components/GamesDisplay.js
+++ frontend/src/components/GamesDisplay.js
@@ -1,15 +1,24 @@
 // React imports
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 // Component imports
 
 // Bootstrap imports
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 // Axios imports
 import axiosAuth from '../lib/axios'
 import axios from 'axios'
+import XMLParser from 'react-xml-parser'
+
+// Generic Imports
+import logo from '../images/Logo-Light.png'
 
 export default function GamesDisplay() {
   const [allGames, setAllGames] = useState([])
+  const [topGames, setTopGames] = useState([])
 
   useEffect(() => {
     async function getGamesData() {
@@ -24,12 +33,54 @@ export default function GamesDisplay() {
     getGamesData()
   }, [])
 
+  useEffect(() => {
+    async function getTopGamesData() {
+      try {
+        // const { data } = await axiosAuth.get('/api/games/') // This is authorised route for testing.
+        const { data } = await axios.get('https://boardgamegeek.com/xmlapi2/hot?type=boardgame&count=10') // This is unauthorised for testing
+        const convertedData = new XMLParser().parseFromString(data)
+        console.log(convertedData.children)
+        setTopGames(convertedData.children)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTopGamesData()
+  }, [])
+
   return (
     <section className='display-background'>
-      {allGames.length > 0 ?
-        <h1>{allGames[1].name}</h1> :
-        <h1>Loading...</h1>
-      }
+      <div className='display'>
+        <Row className='d-flex'>
+          <Col xs={0} md={0} lg={2} className='left-col'>
+            <Link to={''}>
+              <img className='logo' src={logo} alt="collect-dot-bg-logo" />
+            </Link>
+            {topGames.map((game, index) => {
+              return (
+                <Fragment key={index}>
+                </Fragment>)
+            })}
+            <p>Socials</p>
+          </Col>
+          <Col xs={12} md={9} lg={7} className='center-col'>
+            <p>Carousel</p>
+            <p>Categories</p>
+            <p>Games Display</p>
+            {allGames.length > 0 ?
+              <p>{allGames[1].name}</p> :
+              <p>Loading...</p>
+            }
+          </Col>
+          <Col xs={0} md={3} lg={3} className='right-col'>
+            <p>Profile</p>
+            <p>Feature</p>
+            <p>Chat/Friend</p>
+          </Col>
+        </Row>
+
+
+      </div>
     </section>
   )
 }
